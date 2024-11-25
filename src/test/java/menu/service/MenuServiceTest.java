9c419d135479853793a68cce5714a03b3bc2e26b
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-25 21:47:31 +0900

    test: 카테고리 선택 테스트코드 작성

---
+++ src/test/java/menu/service/MenuServiceTest.java
@@ -0,0 +1,40 @@
+package menu.service;
+
+import menu.domain.Category;
+import menu.domain.CategoryResult;
+import menu.utils.InputParser;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class MenuServiceTest {
+
+    private MenuService menuService = new MenuService(InputParser.create(), CategoryResult.create());
+
+    @RepeatedTest(5)
+    void test() {
+        Category category1 = menuService.recommendCategory();
+        Category category2 = menuService.recommendCategory();
+        Category category3 = menuService.recommendCategory();
+        Category category4 = menuService.recommendCategory();
+        Category category5 = menuService.recommendCategory();
+        Category category6 = menuService.recommendCategory();
+        Category category7 = menuService.recommendCategory();
+        Category category8 = menuService.recommendCategory();
+        Category category9 = menuService.recommendCategory();
+        Category category10 = menuService.recommendCategory();
+
+        System.out.println(category1);
+        System.out.println(category2);
+        System.out.println(category3);
+        System.out.println(category4);
+        System.out.println(category5);
+        System.out.println(category6);
+        System.out.println(category7);
+        System.out.println(category8);
+        System.out.println(category9);
+        System.out.println(category10);
+
+
+
+    }
+
+}
